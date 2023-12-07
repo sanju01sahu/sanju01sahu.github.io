aa5552fc83a9429e9f93b838d560787dfc3a083f
Sanjay Sahu <77436131+sanju01sahu@users.noreply.github.com>
2023-12-07 11:26:05 +0530

    update

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+// import {Box, Flex} from "@chakra-ui/react"
+import styled from "styled-components";
 
 const Navbar = () => {
   return (
-    <div id="nav-menu">
-      
-      <a className="nav-link home" href="#home">
+    <DIV>
+      <div id="nav-menu">
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+        <a className="nav-link home" href="#home">
         Home
       </a>
       <a className="nav-link about" href="#about">
@@ -19,11 +25,57 @@ const Navbar = () => {
       <a className="nav-link contact" href="#contact">
         Contacts
       </a>
-      <a className="nav-link resume" href="#resume">
+      <a id="resume-button-2" className="nav-link resume" href="#resume">
         Resume
       </a>
-    </div>
+        </div>
+      </div>
+    </DIV>
   );
 };
 
 export default Navbar;
+
+const DIV = styled.div`
+  
+  #nav-menu {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin: auto;
+    width: 100%;
+    height: 80px;
+    overflow: hidden;
+    box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+
+  }
+  #nav-menu > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin: auto;
+    width: 90%;
+    height: 80px;
+    padding: 0px 50px 0px 0px;
+    margin: 0px 50px 0px -30px;
+  }
+  #nav-menu > div > a{
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+    border-radius: 5px;
+    text-transform:uppercase;
+    
+  }
+
+  #nav-menu > div > a:hover {
+    background-color: #f6f6f6;
+    color: black;
+  }
+  
+  #nav-menu > div>  a.active {
+    background-color: #04AA6D;
+    color: white;
+  }
+
+`
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import styled from "styled-components";
 
 export const Home = () => {
   return (
-    <div id="home" >
+    <DIV>
+      <div id="home" >
         <div>
             <h2 id="user-detail-name" >Hello, I am Sanjay Sahu.</h2>
             <p></p>
             <p  id="user-detail-intro" >Outcome-driven Full Stack Web Developer. Proficient in MERN stack, showing intermediate-level skills in Data Structures and Algorithms. Accomplished in both solo and collaborative projects, blending technical prowess with effective teamwork. Adept in designing and optimizing server-side APIs with Node.js and Express.js, crafting responsive web applications using React.js, and proficient in MongoDB for efficient database operations.</p>
             <span>
             <a href="#"><img src="#" alt="" /></a>
-            <a href="#"><img src="#" alt="" /></a>
-            <a href="#"><button  ></button></a>
+            <a href="https://sanju01sahu.github.io/"><img src="" alt="" /></a>
+            <a href="#"><button id='resume-button-1'  >Resume</button></a>
             </span>
         </div>
         <div>
@@ -18,5 +20,15 @@ export const Home = () => {
         </div>
         
     </div>
+    </DIV>
   )
 }
+
+const DIV = styled.div`
+  #home div{
+    width:80%;
+    margin auto;
+    display:flex;
+    flex-direction:column;
+  }
+`
